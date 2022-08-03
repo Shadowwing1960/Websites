ec95f8508464346988e7c7e83d51895dc7433a1f
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2022-08-03 19:33:33 +1200

    Columns for table of data base added
    - Identification of each column and what they are defined

---
+++ Website/model.py
@@ -0,0 +1,21 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+# Objects related to To-do-list
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+# Objects related to database
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+ 
