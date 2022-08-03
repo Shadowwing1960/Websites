dc734c832117ebbba6f4c3428df490be044c5d28
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2022-08-03 19:35:11 +1200

    Login URL / Function added

---
+++ Website/auth.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__)
+
+
+# login function / route
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    # Getting email and password from form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Authorising User
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+  
