b7aac05f33bd0dcb48ca67b59882f825a9be16db
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2022-08-03 19:41:40 +1200

    Added delete note java script
    - redirect user to the same URL after deleting a note from To Do List

---
+++ Website/static/index.js
@@ -0,0 +1,7 @@
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/todolist";
+  });
