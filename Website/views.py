bc8af7c29ff9d36d5535494a4aefb188d88e762e
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2022-08-03 19:25:31 +1200

    To Do list URL and functions added and

--- Website/views.py
+++ Website/views.py
@@ -12,3 +12,30 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
   
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+  
+@views.route('/todolist', methods=['GET', 'POST'])
+def to_do_list():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Task is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Task added!', category='success')
+
+    return render_template("to_do_list.html", user=current_user)
+
