a2bc59404544b1aaf21e1308ce02d0ba27c5d4c3
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2022-08-10 22:56:37 +1200

    simple comments added

--- Website/views.py
+++ Website/views.py
@@ -10,12 +10,14 @@ import sys
 views = Blueprint('views', __name__)
 
 
+# Home URL
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     return render_template("home.html", user=current_user)
 
 
+# Deleting a note from To Do List URl
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
     note = json.loads(request.data)
@@ -29,6 +31,7 @@ def delete_note():
     return jsonify({})
 
 
+# Open GUI Note Pad in website as a URL
 @views.route('/notepad')
 def notepad():
     def redirect_home():
@@ -42,12 +45,13 @@ def notepad():
     from Notes.Note_Pad_Code.Note_Pad import note_pad
     notes = threading.Thread(target=note_pad, name='notes')
     redirecting = threading.Thread(target=redirect_home, name='redirecting')
+
     # Running Gui Note Pad
     redirecting.start()
     notes.start()
     return render_template("home.html", user=current_user)
 
-
+# To do list URL
 @views.route('/todolist', methods=['GET', 'POST'])
 def to_do_list():
     if request.method == 'POST':
@@ -64,6 +68,7 @@ def to_do_list():
     return render_template("to_do_list.html", user=current_user)
 
 
+# Contact us URl
 @views.route('/contact-us', methods=['GET', 'POST'])
 def contact():
     return render_template("contact_us.html", user=current_user)
