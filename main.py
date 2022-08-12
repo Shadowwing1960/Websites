e18059f73e9c793dd0c4b13a008565cca5e06b69
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2022-08-12 19:32:06 +1200

    Def name changed and comments added

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from Website import create_app
 
 
-def website_start_up():
+# starting website 
+def website():
     app = create_app()
 
     if __name__ == '__main__':
         app.run(debug=True)
 
 
-website_start_up()
+# Running website
+website()
